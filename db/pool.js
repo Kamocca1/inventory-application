11b38aa5b5ba5f26aa6a3c0af62d741f86fa6a49
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-09-22 16:32:56 +0200

    fix(db): use correct URL for pool connection
    Switched the database connection string from DATABASE_LOCAL_URL to DATABASE_PUBLIC_URL in db/pool.js to ensure the application connects to the correct database environment.

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ import "dotenv/config";
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_LOCAL_URL,
+    connectionString: process.env.DATABASE_PUBLIC_URL,
 });
